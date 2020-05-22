5d61359d717ccc507e1e69069ceff2fdd924a6f7
Monika Messerer <monika.messerer@telekom.de>
2020-05-22 12:16:41 +0200

    initial commit

---
+++ src/main/java/com/wildcodeschool/myStuff/mme/MyStuffApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.myStuff.mme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStuffApplication.class, args);
+	}
+
+}
