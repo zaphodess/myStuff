1e77aedbcc54c9121bcfecca9700d01e47f9e2ba
Monika Messerer <monika.messerer@telekom.de>
2020-05-25 09:19:54 +0200

    redundancy removed

--- src/main/java/com/wildcodeschool/myStuff/mme/repositories/ItemRepository.java
+++ src/main/java/com/wildcodeschool/myStuff/mme/repositories/ItemRepository.java
@@ -1,8 +1,5 @@
 package com.wildcodeschool.myStuff.mme.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +8,5 @@ import com.wildcodeschool.myStuff.mme.entities.Item;
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
 	
-	Optional<Item> findById(Long id);
-	List<Item> findAll();
 
 }
